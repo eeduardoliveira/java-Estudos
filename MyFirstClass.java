2f9c83da8b27fc62b8a3d54af9499a9579f25ae4
Eduardo R. Oliveira <83559192+eeduardoliveira@users.noreply.github.com>
2021-10-26 01:50:16 -0300

    Update MyFirstClass.java
    retiro de public do class

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -1,6 +1,6 @@
 
-public class MyFirstClass{
-    public static void main(String... args){
+ class MyFirstClass{
+    public static void main(String[] args){
         System.out.printIn("Hello, World!");
     }
 }
