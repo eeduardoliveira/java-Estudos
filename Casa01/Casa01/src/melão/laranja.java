a2d54e6adb076ab8df5cc542beb0c67d3b528cd6
Eduardo R. Oliveira <83559192+eeduardoliveira@users.noreply.github.com>
2021-10-28 15:41:46 -0300

    Add files via upload

---
+++ Casa01/Casa01/src/melão/laranja.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package melão;
+
+
+/**
+ *
+ * @author UEPA-LAB6-PC07
+ */
+
+
+public class laranja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[ ] args) {
+        System.out.println ("Hello em println");
+        System.out.printf("hello \n em \n printf");
+        
+        // TODO code application logic here
+    }
+    
+}
+
